3f6172404c5367d5f3ce19507f945ac60350d4c5
Nguyen Le <nathanle89@gmail.com>
2018-11-01 23:30:40 -0700

    Project structure

---
+++ geocoding/app/services/geocoder_service.py
@@ -0,0 +1,9 @@
+import geocoder
+
+class GeoCoderService:
+
+    @classmethod
+    def lookup(cls, latitude, longitude):
+        #TODO
+        pass
+        # results = geocoder.google([latitude, longitude], method='reverse')
---
+++ geocoding/config/settings/development.py
@@ -0,0 +1,4 @@
+from .base import *
+
+SECRET_KEY = '7(v+k*h5vxfbhb+4w3g^sdni5(%qt$^rlb($rsq_acs)ms4&j4'
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
